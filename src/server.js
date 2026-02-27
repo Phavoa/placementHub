bd664e9a72c38f3a543c4146cf69e5d435e68e71
Efemiaya Favour Tega <efemiayafavour@gmail.com>
2026-02-27 16:53:26 +0100

    chore:

--- src/server.js
+++ src/server.js
@@ -5,8 +5,12 @@ import { env } from "./config/env.js";
 const startServer = async () => {
   await connectDB();
 
-  app.listen(env.port, () => {
+  app.listen(env.port, "0.0.0.0", () => {
     console.log(`Server running on port ${env.port}`);
+    console.log(`Local Access: http://localhost:${env.port}`);
+    console.log(
+      `Network Access: http://0.0.0.0:${env.port} (Use your computer's IP address)`,
+    );
   });
 };
 
